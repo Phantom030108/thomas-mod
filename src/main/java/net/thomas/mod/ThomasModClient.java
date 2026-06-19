959743120b92fcc9e703412d74b07ac8b78b5275
Thomas <thomasmontalto12@gmail.com>
2026-06-19 14:20:13 +0200

    First Commit

---
+++ src/main/java/net/thomas/mod/ThomasModClient.java
@@ -0,0 +1,10 @@
+package net.thomas.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThomasModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
