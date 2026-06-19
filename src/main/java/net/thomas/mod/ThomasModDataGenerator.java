4a5107849dd7050e2248bdfa14618a740c623c59
Thomas <thomasmontalto12@gmail.com>
2026-06-19 14:10:55 +0200

    Initial commit

---
+++ src/main/java/net/thomas/mod/ThomasModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thomas.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThomasModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
